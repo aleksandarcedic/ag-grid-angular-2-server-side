84f294cbc941f31824d7d783ef62fe6953b2f823
Aleksandar <aleksandar.cedic@gmail.com>
2016-12-26 17:02:19 +0100

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,10 +23,11 @@ export class AppComponent implements OnInit {
   showGrid: boolean = true;
 
   columnDefs = [
-    { headerName: "Id", field: "id", width: 200, filter: 'number' , filterParams: { newRowsAction: 'keep' }},
+    { headerName: "Id", field: "id", width: 200, filter: 'number', filterParams: { newRowsAction: 'keep' } },
     { headerName: "First name", field: "first_name", width: 200, filter: 'text', filterParams: { newRowsAction: 'keep' } },
     { headerName: "Last name", field: "last_name", width: 200, filter: 'text', filterParams: { newRowsAction: 'keep' } }
   ]
+
   listData = [];
   title = 'app works!';
   p: number = 1;
@@ -36,72 +37,39 @@ export class AppComponent implements OnInit {
     paginationPageSize: this.defaultSizePerPage,
   };
 
-
   constructor(private http: Http) { }
+
   dataSource = {
-
-
-
     getRows: (params: any) => {
-
-      console.log(params);
-      console.log("defaultSizePerPage " + this.gridOptions.paginationPageSize);
-      console.log("quickFilter=" + this.quickFilter);
-      console.log("page=" + (params.endRow / this.gridOptions.paginationPageSize));
       var sortString;
-
       var sortState = this.gridOptions.api.getSortModel();
       if (sortState.length !== 0) {
-        console.log(sortState[0]);
         sortString = sortState[0].colId + "," + sortState[0].sort;
       }
-
-      if(this.quickFilter){
+      if (this.quickFilter) {
         var useQuickFilter = this.quickFilter;
       }
-
       var filterState = this.gridOptions.api.getFilterModel();
       var filterSend = [];
- for (var k in filterState){
-    if (filterState.hasOwnProperty(k)) {
-         filterSend.push({column:k,filter:filterState[k].filter, type:filterState[k].type});
-    }
-}
-console.log(JSON.stringify( filterSend ));
-
-
-
-
-
-
+      for (var k in filterState) {
+        if (filterState.hasOwnProperty(k)) {
+          filterSend.push({ column: k, filter: filterState[k].filter, type: filterState[k].type });
+        }
+      }
       var getPage = params.endRow / this.gridOptions.paginationPageSize;
-      this.serverCall(this.gridOptions.paginationPageSize, getPage, sortString, useQuickFilter,filterSend ).subscribe(
+      this.serverCall(this.gridOptions.paginationPageSize, getPage, sortString, useQuickFilter, filterSend).subscribe(
         result => {
-
-
           var rowsThisPage = result.data;
-
           var lastRow = result.totalElements;
-
           params.successCallback(rowsThisPage, lastRow);
         },
         error => this.errorMessage = <any>error);
-
-
-
     }
   }
-
-
   ngOnInit() {
     this.gridOptions.datasource = this.dataSource;
   }
-
-
-
-
   serverCall(rowsPerPage: number, page: number, sort?: string, quickFilter?: string, filterPerColumn?: Array<any>): Observable<IServerResponse> {
-
     let params: URLSearchParams = new URLSearchParams();
     params.set('page', page.toString());
     params.set('size', rowsPerPage.toString());
@@ -110,35 +78,21 @@ console.log(JSON.stringify( filterSend ));
       params.set('sort', sort);
     }
 
-    if(quickFilter){
-      params.set('quickFilter',quickFilter);
+    if (quickFilter) {
+      params.set('quickFilter', quickFilter);
     }
 
-    if(filterPerColumn.length>0){
-      params.set('filterPerColumn', JSON.stringify( filterPerColumn ));
+    if (filterPerColumn.length > 0) {
+      params.set('filterPerColumn', JSON.stringify(filterPerColumn));
     }
 
-    return this.http.get("http://localhost/blog/pag2.php", { search: params })
+    return this.http.get("http://localhost/pag.php", { search: params })
       .map((response: Response) => response.json())
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
   }
-
-
-
-
-
-
-
-
-
-
-
   onChange(rowsPerPage) {
-
     this.gridOptions.paginationPageSize = parseInt(rowsPerPage);
-
     this.gridOptions.api.setDatasource(this.dataSource);
-
   }
   private onRowClicked($event) {
     console.log('onRowClicked: ' + $event.node.data.userNumber);
